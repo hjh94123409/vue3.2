ba5c2954ee7047ddc1c921ab4bca7be2518618cf
jianhua1 <94123409@qq.com>
2022-07-06 23:44:22 +0800

    自定义hooks 自定义插件 全局函数和变量

---
+++ 25_Directive/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import mitt from 'mitt'
+
+const Mit = mitt()
+
+const app = createApp(App)
+
+declare module 'vue' {
+    export interface ComponentCustomProperties{
+        $Bus: typeof Mit
+    }
+}
+
+app.config.globalProperties.$Bus = Mit
+
+app.mount('#app')
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import mitt from 'mitt'
 
-const Mit = mitt()
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
-declare module 'vue' {
-    export interface ComponentCustomProperties{
-        $Bus: typeof Mit
-    }
-}
-
-app.config.globalProperties.$Bus = Mit
+app.use(ElementPlus)
 
 app.mount('#app')
