7028f69455cd18331a85d0c6f3301c64b10efc54
hehele <94123409@qq.com>
2022-07-11 09:09:42 +0800

    测试

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,11 @@ const Mit = mitt()
 const app = createApp(App)
 
 declare module 'vue' {
-    export interface ComponentCustomProperties{
+    export interface ComponentCustomProperties {
         $Bus: typeof Mit
     }
 }
+
 
 app.config.globalProperties.$Bus = Mit
 
